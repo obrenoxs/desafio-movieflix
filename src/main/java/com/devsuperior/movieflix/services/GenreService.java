5264639114ba24499c93741ccba4b93e61c56497
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-18 08:42:29 -0300

    feat: implement genre listing endpoint

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GenreService {
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Transactional(readOnly = true)
+    public List<GenreDTO> findAll() {
+        List<Genre> list = genreRepository.findAll();
+        List<GenreDTO> listDTO = list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+        return listDTO;
+    }
+}
