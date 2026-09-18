95b5e6a8beca923555c9a2610af38f39ebe23fe8
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-18 10:22:23 -0300

    feat: implement review creation with member-only access and validation

--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -1,5 +1,6 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Review;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -16,7 +17,19 @@ public class ReviewDTO {
     private Long userId;
     private String userName;
     private String userEmail;
-    
+
+	public ReviewDTO() {
+	}
+
+	public ReviewDTO(Review review) {
+		id = review.getId();
+		text = review.getText();
+		movieId = review.getMovie().getId();
+		userId = review.getUser().getId();
+		userName = review.getUser().getName();
+		userEmail = review.getUser().getEmail();
+	}
+
 	public Long getId() {
 		return id;
 	}
---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Transactional
+    public ReviewDTO insert(ReviewDTO request) {
+
+        Movie movie = movieRepository.findById(request.getMovieId())
+                .orElseThrow(() -> new ResourceNotFoundException("Filme não existe"));
+
+        User user = authService.authenticated();
+
+        Review review = new Review();
+        review.setText(request.getText());
+        review.setUser(user);
+        review.setMovie(movie);
+
+        review = reviewRepository.save(review);
+
+        return new ReviewDTO(review);
+    }
+}
